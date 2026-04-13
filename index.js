83d8c8b5b1fd5ddd504db6c30c41471fba5ded3b
Datadan82 <hamzahafez@gmail.com>
2026-04-12 21:38:07 -0400

    fix: revert to startStandaloneServer to fix Render deploy

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
 const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const { startStandaloneServer } = require('@apollo/server/standalone');
 const connectDB = require('./config/db');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
@@ -11,34 +8,17 @@ const resolvers = require('./graphql/resolvers');
 async function startServer() {
   connectDB();
 
-  const app = express();
-
   const server = new ApolloServer({
     typeDefs,
     resolvers,
   });
 
-  await server.start();
-
-  app.use(
-    '/graphql',
-    cors({
-      origin: [
-        'http://localhost:4200',
-        'https://101429091-comp3133-assignment2-wkcv.vercel.app'
-      ],
-      credentials: true
-    }),
-    bodyParser.json(),
-    expressMiddleware(server, {
-      context: async ({ req }) => ({ req })
-    })
-  );
-
-  const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/graphql`);
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: process.env.PORT || 4000 },
+    context: async ({ req }) => ({ req }),
   });
+
+  console.log(`Server running at ${url}`);
 }
 
 startServer();
