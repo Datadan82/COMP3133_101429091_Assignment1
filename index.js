96371cdcf4e1cbe7e0a8f3e492d777a89f9e3310
Datadan82 <hamzahafez@gmail.com>
2026-04-12 21:30:08 -0400

    fix: add CORS for Vercel frontend

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
+const { expressMiddleware } = require('@apollo/server/express4');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
@@ -8,17 +11,34 @@ const resolvers = require('./graphql/resolvers');
 async function startServer() {
   connectDB();
 
+  const app = express();
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
   });
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-    context: async ({ req }) => ({ req }),
-  });
+  await server.start();
 
-  console.log(`Server running at ${url}`);
+  app.use(
+    '/graphql',
+    cors({
+      origin: [
+        'http://localhost:4200',
+        'https://101429091-comp3133-assignment2-wkcv.vercel.app'
+      ],
+      credentials: true
+    }),
+    bodyParser.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ req })
+    })
+  );
+
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/graphql`);
+  });
 }
 
 startServer();
